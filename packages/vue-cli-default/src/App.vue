a1ba3e2a6444f1b74d15d87c29febd430304abea
Frank van Wijk <fvanwijk@xebia.com>
2018-04-06 14:26:23 +0200

    Copy app stub to vue-cli-default

--- packages/vue-cli-default/src/App.vue
+++ packages/vue-cli-default/src/App.vue
@@ -1,28 +1,77 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
+    <main>
+      <header class="main-header">
+        <h1>CLI performance metrics</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+          Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+        </p>
+      </header>
+
+      <table class="measurement-table">
+        <thead>
+          <tr>
+            <td></td>
+            <td><a href="/">Angular</a></td>
+            <td><a href="/">React</a></td>
+            <td><a href="/">VueJS</a></td>
+            <td><a href="/">Nuxt.js</a></td>
+            <td><a href="/">Polymer</a></td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Lighthouse Performance</td>
+            <td>...</td>
+            <td>...</td>
+            <td>...</td>
+            <td>...</td>
+            <td>...</td>
+          </tr>
+          <tr>
+            <td>First Meaningful Paint</td>
+            <td>...</td>
+            <td>...</td>
+            <td>...</td>
+            <td>...</td>
+            <td>...</td>
+          </tr>
+          <tr>
+            <td>First Interactive</td>
+            <td>...</td>
+            <td>...</td>
+            <td>...</td>
+            <td>...</td>
+            <td>...</td>
+          </tr>
+        </tbody>
+      </table>
+    </main>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import './main.css';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  div {
+    height: 100%;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
