37cb56111e5dc04cdd0c14b433c12b46827e9a47
Ruben Oostinga <ruben.oostinga@gmail.com>
2018-04-06 15:44:08 +0200

    React template added

--- packages/create-react-app/src/App.js
+++ packages/create-react-app/src/App.js
@@ -1,21 +1,114 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import report from './report.json';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+const MeasurementRow = ({ metric }) => (
+  <tr>
+    <td>{metric}</td>
+    {Object.keys(report).map(key => (
+      <td>
+        {
+          report[key].metrics.find(({ description }) => description === metric)
+            .displayValue
+        }
+      </td>
+    ))}
+  </tr>
+);
+
+export default () => (
+  <div>
+    <main>
+      <header className="main-header">
+        <h1>CLI performance metrics</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
+          felis, ultricies nec, pellentesque eu, pretium quis, sem.
         </p>
-      </div>
-    );
-  }
-}
+      </header>
+      <table className="measurement-table">
+        <thead>
+        <tr>
+          <td />
+          {Object.keys(report).map(key => (
+            <td>
+              <a
+                href={`https://${key}-dot-ruben-oostinga-speeltuin.appspot.com`}
+              >
+                {report[key].name}
+              </a>
+            </td>
+          ))}
+        </tr>
+        </thead>
+        <tbody>
+        {report[Object.keys(report)[0]].metrics.map(({ description }) => (
+          <MeasurementRow metric={description} />
+        ))}
+        </tbody>
+      </table>
+    </main>
+    <footer className="main-footer">
+      Made with ❤️ @ <a href="https://xebia.com/">Xebia</a>
+    </footer>
+    <style jsx>{`
+      main {
+        flex: 1;
+        max-width: 800px;
+        margin: auto;
+        padding: 50px 0 0;
+      }
 
-export default App;
+      .main-header {
+        margin-bottom: 50px;
+        text-align: center;
+      }
+
+      .main-footer {
+        height: 50px;
+        text-align: center;
+        font-size: 12px;
+      }
+
+      .measurement-table {
+        margin: auto;
+        border: 1px solid #eeeeee;
+        border-spacing: 0;
+        border-collapse: collapse;
+      }
+
+      .measurement-table thead {
+        background-color: #6f3a6f;
+      }
+
+      .measurement-table thead a {
+        color: #fff;
+      }
+
+      .measurement-table tr {
+        border-bottom: 1px solid #eeeeee;
+      }
+
+      .measurement-table td {
+        padding: 10px 20px;
+        text-align: center;
+      }
+
+      .measurement-table td:first-child {
+        text-align: left;
+      }
+    `}</style>
+    <style global jsx>{`
+      body,
+      html {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        margin: 0;
+        font-family: Helvetica Neue, Helvetica, Roboto, Arial, sans-serif;
+      }
+    `}</style>
+  </div>
+);
