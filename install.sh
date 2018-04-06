19512628bdae2e111f50cc844aa46ed8d8d4825a
Frank van Wijk <fvanwijk@xebia.com>
2018-04-06 11:12:53 +0200

    Add install script because workspaces are not working because of hoisting

---
+++ install.sh
@@ -0,0 +1,20 @@
+cd packages/angular-cli
+npm install
+
+cd ../create-nuxt-app
+npm install
+
+cd ../create-react-app
+npm install
+
+cd ../polymer-2-application
+bower install
+
+cd ../vue-cli-default
+npm install
+
+cd ../vue-cli-nuxt
+npm install
+
+cd ../vue-cli-simple-with-prettier
+npm install
