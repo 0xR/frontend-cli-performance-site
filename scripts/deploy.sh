7ae5b0178696b76a65897a71c357cf6b9210079e
Ruben Oostinga <ruben.oostinga@gmail.com>
2018-05-12 13:11:10 +0200

    Skip deploy for unchanged files

---
+++ scripts/deploy.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if !([ $# = 1 ]  || [ $# = 2 ])
+then
+    echo "Usage ./script/deploy.sh {package} [{version}]"
+    exit -1
+fi
+
+SERVICE="$1"
+PACKAGE_DIR="$DIR/../packages/$1"
+
+if ! [ -d "${PACKAGE_DIR}" ]
+then
+    echo "$1 is not a valid package"
+    exit -2
+fi
+
+if [ $# = 1 ]
+then
+    VERSION_PREFIX=local
+else
+    VERSION_PREFIX="$2"
+fi
+
+cd $DIR/..
+
+HASH=$(git ls-files "${PACKAGE_DIR}" | xargs cat | sha1sum - | awk '{print $1}' | cut -c-7)
+VERSION="${VERSION_PREFIX}-${HASH}"
+
+if gcloud app versions list -s "${SERVICE}" | grep -q "${HASH}"
+then
+    echo "Skipping deploy, hash ${HASH} already built for service ${SERVICE}"
+else
+    cd "${PACKAGE_DIR}"
+    gcloud -q app deploy --version "${VERSION}"
+fi
