00b67af8091a0d2be9f30c94a7efafd3f84f14a5
Ruben Oostinga <ruben.oostinga@gmail.com>
2018-05-12 20:24:00 +0200

    Make ng-cli work

--- packages/angular-cli/src/app/app.module.ts
+++ packages/angular-cli/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { KeysPipe } from './keys.pipe';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KeysPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
