5178ad61cd59c726b431925e10ac2242a96a5eaa
Ruben Oostinga <ruben.oostinga@gmail.com>
2018-05-12 18:44:23 +0200

    Automatically clean unused versions

--- scripts/clean-versions.sh
+++ scripts/clean-versions.sh
@@ -1,22 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 
-function clean_versions {
-    VERSIONS=$(gcloud app versions list --service $1 --sort-by '~version' --format 'value(version.id)')
-    COUNT=0
-    echo "Keeping the $2 latest versions of the $1 service"
-    for VERSION in $VERSIONS
-    do
-        ((COUNT++))
-        if [ $COUNT -gt $2 ]
-        then
-          echo "Going to delete version $VERSION of the $1 service."
-          gcloud app versions delete $VERSION --service $1 -q
-        else
-          echo "Going to keep version $VERSION of the $1 service."
-        fi
-    done
+if [ $# != 1 ]
+then
+    echo "Usage ./script/clean-versions.sh {package}"
+    exit -1
+fi
 
-}
+gcloud app versions list --filter="traffic_split!=1" -s "$1" --format="value(version.id)" | xargs gcloud -q app versions delete
 
-#gcloud app services list --format 'value(id)' | while read service; do clean_versions ${service} 2; done
-gcloud app services list --format 'value(id)' | grep -v default |  while read service; do gcloud app versions delete test --service ${service} -q; done
